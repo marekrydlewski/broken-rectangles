36393063ae81b40c314a76b2336909941658fe16
Marek Rydlewski <marek.rydlewski@outlook.com>
2017-04-08 20:19:54 +0200

    Get sides coverage in work

--- main.py
+++ main.py
@@ -4,20 +4,36 @@ import matplotlib.pyplot as plt
 import imutils
 
 
-def get_formatted_image(img):
+def get_sides_coverage(img):
     ret, thresh = cv2.threshold(img, 127, 255, 0)
     im2, contours, hierarchy = cv2.findContours(thresh, 1, 2)
     cnt = contours[0]
     rect = cv2.minAreaRect(cnt)
-    # img = cv2.drawContours(img, [box], 0, (125, 0, 255), 2)
-    # plt.imshow(img)
-    # plt.show()
+    box = cv2.boxPoints(rect)
+    tops = np.int0(box)
+
+    tops2 = tops.append(tops[0])
+    tops2 = tops2[1:]
+    tops_lines = [(a, b) for a, b in zip(tops, tops2)]
+    # pairs of points creating a line
+
+    for line in tops_lines:
+        pass
+
+
+def get_formatted_image(img):
+    ret, thresh = cv2.threshold(img, 127, 255, 0)
+    im2, contours, hierarchy = cv2.findContours(thresh, 1, 2)
+    cnt = contours[0]
 
     # rotacja
+    rect = cv2.minAreaRect(cnt)
     angle = 180 - rect[2] + 90  # trzeba dobrac odpowiedni kat
     img = imutils.rotate_bound(img, angle=angle)
 
-    # edges = cv2.Canny(img, 50, 150, apertureSize=3)
+    # find tops
+    coverages = get_sides_coverage(img)
+
 
     # lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
     # for rho, theta in lines[0]:
